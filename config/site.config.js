52d559c65063ff810b206f1d7792e61c0f2d405c
iceball1 <hamasaki.vn@gmail.com>
2024-02-09 18:34:14 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/foldershare', 'testthupasswordfolder'],
+  protectedRoutes: ['/phim', 'testthupasswordfolder'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
